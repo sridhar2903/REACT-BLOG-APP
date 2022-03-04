b6330c40c29e22a8e2a98a75941d6c7bb3d69278
sridhar2903 <saisridhard@gmail.com>
2022-03-04 14:20:00 +0530

    changes

--- src/Components/Homepage/Home.js
+++ src/Components/Homepage/Home.js
@@ -4,21 +4,21 @@ import TopPostsDetails from './TopPostsDetails'
 import './stylehome.css'
 // OBJECTS RELATED TO LATEST IN HOME PAGE
 const latestone = {
-  image: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='aerobic' />,
+  image: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='aerobic' alt='notfound' />,
   title: "Aerobic",
   info: "Aerobic exercise is any type of cardiovascular conditioning. It can include activities like brisk walking, swimming, running, or cycling. You probably know it as cardio.",
   subinfo: <div id="sub-one" ><b>Fitness</b> / January 22, 2022 </div>
 }
 
 const latesttwo = {
-  image: <img src="https://images.indianexpress.com/2021/12/RRR-trailer-launch-live-blog-1200.jpg?w=389" id="rrr" />,
+  image: <img src="https://images.indianexpress.com/2021/12/RRR-trailer-launch-live-blog-1200.jpg?w=389" id="rrr"  alt='notfound'/>,
   title: "RRR Movie",
   info: "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
   subinfo: <div id="sub-two" ><b>Tollywood</b> / March 25, 2022 </div>
 }
 
 const latestthree = {
-  image: <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2021/07/paneer-tikka-restaurant-style.jpg" id="ptikka" />,
+  image: <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2021/07/paneer-tikka-restaurant-style.jpg" id="ptikka" alt='notfound'/>,
   title: "Paneer Tikka",
   info: "Paneer Tikka is a popular and delicious tandoori snack where paneer (Indian cottage cheese cubes) are marinated in a spiced yogurt-based marinade, arranged on skewers and grilled in the oven.",
   subinfo: <div id="sub-three" ><b>Food</b> / January 10, 2022 </div>
@@ -27,35 +27,35 @@ const latestthree = {
 // OBJECTS RELATED TO TOP POST DETAILS IN HOME PAGE
 
 const toppostsdetailsone={
-  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' />,
+  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' alt='notfound' />,
   titletext:<b>Balance</b>,
   maintitle:<div id="main-one-toppost" ><b>Fitness</b> / January 22, <br/>2022 </div>
 
 
 }
 const toppostsdetailstwo={
-  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' />,
+  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' alt='notfound' />,
   titletext:<b>Balance</b>,
   maintitle:<div id="main-one-toppost" ><b>Fitness</b> / January 22, <br/>2022 </div>
 
 
 }
 const toppostsdetailsthree={
-  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' />,
+  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost'  alt='notfound'/>,
   titletext:<b>Balance</b>,
   maintitle:<div id="main-one-toppost" ><b>Fitness</b> / January 22, <br/>2022 </div>
 
 
 }
 const toppostsdetailsfour={
-  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' />,
+  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' alt='notfound' />,
   titletext:<b>Balance</b>,
   maintitle:<div id="main-one-toppost" ><b>Fitness</b> / January 22, <br/>2022 </div>
 
 
 }
 const toppostsdetailsfive={
-  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' />,
+  img: <img src="https://media.istockphoto.com/photos/unrecognizable-sportswoman-jumping-rope-on-a-bridge-picture-id1128646064?b=1&k=20&m=1128646064&s=170667a&w=0&h=laGklQ5LOZMQXFr_ydlDQqK5yRAerjh6AtC3fsaZZ8w=" id='imgpost' alt='notfound' />,
   titletext:<b>Balance</b>,
   maintitle:<div id="main-one-toppost" ><b>Fitness</b> / January 22, <br/>2022 </div>
 
