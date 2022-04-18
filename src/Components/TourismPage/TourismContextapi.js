ee3e1937776d7cc1a365053ce5ddfb5515e2a779
sridhar2903 <saisridhard@gmail.com>
2022-04-18 14:24:10 +0530

    data changes

--- src/Components/TourismPage/TourismContextapi.js
+++ src/Components/TourismPage/TourismContextapi.js
@@ -1,5 +1,5 @@
 import React, {createContext, useState } from "react";
-import kerala from '../Homepage/imagesforHome/kerala.jpg'
+
 export const Tourismcontext = createContext();
 export const TourismProvider=(props)=>
 {
